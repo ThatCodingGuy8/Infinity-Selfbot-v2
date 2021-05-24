b8acd7c596b88bc64428f07450ab76808883305b
ThatCodingGuy8 <30600514+ThatCodingGuy8@users.noreply.github.com>
2021-05-24 14:12:18 -0400

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -2,13 +2,13 @@ const figlet = require("figlet");
 
 module.exports = async (client) => {
 
-    let mesg = "Aesthetix SB"
+    let mesg = "Infinity Selfbot"
 
     figlet(mesg, async (err, ascii) => {
         if (err) {
             return;
         }
         console.log(ascii)
-        await console.log(`${client.user.tag} Logged In, Have fun using Aesthetix`)
+        await console.log(`${client.user.tag} Logged In, Have fun using Infinity!`)
     })
 }
