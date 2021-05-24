ca31cdcf490540e4e529b39d444aafe0d80134df
ThatCodingGuy8 <30600514+ThatCodingGuy8@users.noreply.github.com>
2021-05-24 17:45:28 -0400

    Fixed NSFW

--- commands/NSFW/uniform.js
+++ commands/NSFW/uniform.js
@@ -10,7 +10,7 @@ module.exports = {
      * @param {msg} msg
      */
     async execute(msg, args) {
-        if (msg.channel.type != "dm" || !msg.channel.nsfw) return msg.channel.send(new MessageEmbed().setTitle("HiHi! This is Self-Bot-Chan~~ in your service!").setDescription("This is not a nsfw channel nor is it in dms, Master!"))
+        if (msg.channel.type != "dm" && !msg.channel.nsfw) return msg.channel.send(new MessageEmbed().setTitle("HiHi! This is Self-Bot-Chan~~ in your service!").setDescription("This is not a nsfw channel nor is it in dms, Master!"))
  nsfw = await akaneko.nsfw
  
    msg.channel.send(new MessageEmbed().setDescription("Here you go Master!").setImage(await nsfw.uniform()))
