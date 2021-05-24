ed19bf3f4c472cd2c8872b8a56ec425d7a24c689
ThatCodingGuy8 <30600514+ThatCodingGuy8@users.noreply.github.com>
2021-05-24 18:47:52 -0400

    Fixed reddit

--- commands/Fun/reddit.js
+++ commands/Fun/reddit.js
@@ -1,4 +1,4 @@
-const { Message, MessageEmbed } = require('discord.js-selfbot');
+const { msg, msgEmbed } = require('discord.js-selfbot');
 const settings = require("./../../settings.json");
 const Discord = require("discord.js-selfbot")
 const axios = require('axios')
@@ -8,16 +8,16 @@ module.exports = {
     usage: 'reddit <subreddit>',
     aliases: ['r'],
     async execute(msg, args) {
-        if (!args) return message.channel.send("Give me the subreddit name!");
+        if (!args) return msg.channel.send("Give me the subreddit name!");
         const url = `https://www.reddit.com/r/${args.join("")}/hot/.json?limit=100`;
         axios.get(url).then(function(response) {
-                if (response.status == 403) return message.channel.send("Subreddit is private!");
-                if (response.status == 404) return message.channel.send("Subreedit not found!");
+                if (response.status == 403) return msg.channel.send("Subreddit is private!");
+                if (response.status == 404) return msg.channel.send("Subreedit not found!");
 
                 var response = response.data;
-                if (!response.data.children.length) return message.channel.send("Subreddit has no posts!");
+                if (!response.data.children.length) return msg.channel.send("Subreddit has no posts!");
                 var index = response.data.children[Math.floor(Math.random() * (response.data.children.length - 1) + random(0, 1))].data
-                if (index.over_18 && !message.channel.nsfw) return message.channel.send("Results are Over 18! make sure to search NSFW subreddits on NSFW channels!")
+                if (index.over_18 && !msg.channel.nsfw) return msg.channel.send("Results are Over 18! make sure to search NSFW subreddits on NSFW channels!")
 
                 var title = index.title
                 var link = 'https://reddit.com' + index.permalink
@@ -26,31 +26,31 @@ module.exports = {
 
                 if (index.post_hint !== 'image') {
                     var text = index.selftext
-                    const textembed = new Discord.MessageEmbed()
+                    const textembed = new Discord.msgEmbed()
                         .setTitle("A random post from " + subRedditName)
                         .setColor("RANDOM")
                         .setDescription(`[${title}](${link})\n\n${text}`)
                         .setURL(`https://reddit.com/${subRedditName}`)
 
-                    return message.channel.send(textembed);
+                    return msg.channel.send(textembed);
                 }
 
                 var image = index.preview.images[0].source.url.replace('&amp;', '&')
-                const imageembed = new Discord.MessageEmbed()
+                const imageembed = new Discord.msgEmbed()
                     .setTitle("A random post from " + subRedditName)
                     .setImage(image)
                     .setColor("RANDOM")
                     .setDescription(`[${title}](${link})\n\n${text}`)
                     .setURL(`https://reddit.com/${subRedditName}`)
-                message.channel.send(imageembed)
+                msg.channel.send(imageembed)
             })
             .catch(function(error) {
                 if (error.toString().includes(403)) {
-                    return message.channel.send("Subreddit is private!");
+                    return msg.channel.send("Subreddit is private!");
                 } else if (error.toString().includes(404)) {
-                    return message.channel.send("Subreddit not found!");
+                    return msg.channel.send("Subreddit not found!");
                 } else {
-                    return message.channel.send("An Error Occured: `" + error + "`")
+                    return msg.channel.send("An Error Occured: `" + error + "`")
                 }
             })
     }
