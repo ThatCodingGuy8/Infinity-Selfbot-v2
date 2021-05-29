7dec1312b4eb02638812663509b3246c5bfa4b3c
RapidFuge <65614381+RapidFuge@users.noreply.github.com>
2021-05-29 10:23:14 +0800

    Please get weis some help
    his getmember function is fucking async bruh look at this image https://grimreaper.wtf/file/h23np2.png please send help down here

--- commands/Images/smrt.js
+++ commands/Images/smrt.js
@@ -6,13 +6,15 @@ module.exports = {
     usage: "smrt <user>",
     async execute(message, args) {
         if (!args[0]) return message.channel.send("Specify a user!!");
-        const user = getMember(message, args[0]);
+        var user = message.mentions.users.first() || getMember(message, args[0]);
         if (!user) return message.channel.send("User not found!");
+        // fuck you synergy im gonna make a dumb code for your ass
+        if (user == getMember(message, args[0])) user = user.user;
 
         const embed = new Discord.MessageEmbed()
             .setTitle(`Smrt!`)
             .setColor("RANDOM")
-            .setImage(`https://api.no-api-key.com/api/v2/smrt?image=${user.user.avatarURL({ format: 'png', size: 512 }).replace(/.gif/g, '.jpeg')}`)
+            .setImage(`https://api.no-api-key.com/api/v2/smrt?image=${user.avatarURL({ format: 'jpg', size: 512 }).replace(/.gif/g, '.jpg')}`)
             .setFooter("My wife left me")
             .setTimestamp()
             return SilentModeSend(embed, message.channel.id, message, "Normal")
