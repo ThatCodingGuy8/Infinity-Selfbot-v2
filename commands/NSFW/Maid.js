cbd0e0bde1b3f9486fe097de1eaf0cfe994be9dc
ThatCodingGuy8 <30600514+ThatCodingGuy8@users.noreply.github.com>
2021-05-29 19:36:03 -0400

    Made NSFW commands smoother

--- commands/NSFW/Maid.js
+++ commands/NSFW/Maid.js
@@ -1,6 +1,7 @@
 const { Message, MessageEmbed } = require('discord.js-self');
 const akaneko = require('akaneko');
 const Functions = require("./../../utils/Functions.js")
+const settings = require("./../../settings.json");
 
 module.exports = {
     name: 'maid',
@@ -10,9 +11,9 @@ module.exports = {
      * @param {msg} msg
      */
     async execute(msg, args) {
-        if (msg.channel.type != "dm" && !msg.channel.nsfw) return Functions.SilentModeSend(new MessageEmbed().setTitle("HiHi! This is Self-Bot-Chan~~ in your service!").setDescription("This is not a nsfw channel nor is it in dms, Master!"), msg.channel.id, msg, "Normal")
- nsfw = await akaneko.nsfw
+        if (msg.channel.type !== "dm" && !msg.channel.nsfw) return Functions.SilentModeSend(new MessageEmbed().setTitle("HiHi! This is Self-Bot-Chan~~ in your service!").setDescription("This is not a nsfw channel nor is it in dms, Master!").setColor("RED"), msg.channel.id, msg, "Normal")
+        let nsfw = akaneko.nsfw
  
-   Functions.SilentModeSend(new MessageEmbed().setDescription("Here you go Master!").setImage(await nsfw.maid()).setFooter("Sexy").setTimestamp(), msg.channel.id, msg, "Normal")
+   await Functions.SilentModeSend(new MessageEmbed().setDescription("Here you go Master!").setImage(await nsfw.maid()).setFooter("Sexy").setTimestamp().setColor(settings.embedcolor), msg.channel.id, msg, "Normal")
 
     }}
