6325c2b1d91d4cd9dac436a02d9c62026e66080a
ThatCodingGuy8 <30600514+ThatCodingGuy8@users.noreply.github.com>
2021-06-01 20:51:43 -0400

    Tried implementing image downloading

--- index.js
+++ index.js
@@ -162,11 +162,11 @@ client.on("message", async msg => {
                             Object.keys(filter).forEach(key => {
                                 if (isNaN(key) && key !== "destinationchannel" && key !== "filtername") {
                                     if (msg.channel.name.includes(key) && filter[key] === true) {
-                                        EvaluateExtension(channelid, key["filtername"] || "None")
+                                        EvaluateExtension(channelid, key["filtername"])
                                     }
                                 } else if (isNaN(key) === false) {
                                     if (msg.channel.id === key && filter[key] === true) {
-                                        EvaluateExtension(channelid, key["filtername"] || "None")
+                                        EvaluateExtension(channelid, key["filtername"])
                                     }
                                 }
                             })
