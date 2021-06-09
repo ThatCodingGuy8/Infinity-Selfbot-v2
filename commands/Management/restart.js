13233fe3cd284917b02d2a16500e7c59eb33b30c
ThatCodingGuy8 <30600514+ThatCodingGuy8@users.noreply.github.com>
2021-06-09 13:15:46 -0400

    Added cool things

--- commands/Management/restart.js
+++ commands/Management/restart.js
@@ -1,6 +1,6 @@
 const { Message, MessageEmbed } = require("discord.js-self")
 const Functions = require("../../utils/Functions.js")
-const { exec } = require("child_process")
+
 module.exports = {
     name: 'restart',
     description: 'Restarts the bot, pretty simple',
@@ -18,9 +18,8 @@ module.exports = {
                 .setTimestamp()
             return Functions.SilentModeSend(embed, msg.channel.id, msg, "Normal")
         }
-        exec("pm2 flush && pm2 reloadLogs", { windowsHide: true }, async (error, stdout, stderr) => {
-            await Functions.SilentModeSend(new MessageEmbed().setTitle("Restarted!").setFooter("Ill be back soon!").setTimestamp(), msg.channel.id, msg, "Normal")
-            process.exit(1)
-        })
+
+        await Functions.SilentModeSend(new MessageEmbed().setTitle("Restarted!").setFooter("Ill be back soon!").setTimestamp(), msg.channel.id, msg, "Normal")
+        process.exit(1)
     }
 }
